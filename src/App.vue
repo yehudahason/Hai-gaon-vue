8c1e3293a1f19ba6a5de1c585abc9963d6d9be84
yehuda hason <yehudaxt@gmail.com>
2025-06-26 17:13:51 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Links from './components/Links.vue'
+import  Circle from './components/Circle.vue'
+</script>
+
+<template>
+  <Links/>
+  
+  
+
+  <RouterView />
+  <Circle/>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Links.vue
@@ -0,0 +1,45 @@
+<template>
+     <div class="links">
+      <a href="./index.html"><i class="fa-solid fa-house"></i></a>
+      <a href="./book.html"> חלק א'</a>
+      <a href="./book-2.html"> חלק ב'</a>
+      <a href="./book-3.html"> חלק ג'</a>
+      <a href="./roe1.html"> פרק הרואה</a>
+
+      <a href="./about.html">אודות</a>
+    </div>
+
+</template>
+
+<script>
+
+
+
+</script>
+
+<style scoped>
+.links {
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 60px;
+  display: flex;
+  color: rgb(250, 236, 236);
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+.links a {
+  font-weight: bold;
+  cursor: pointer;
+  color: rgb(247, 233, 233);
+  margin: 10px 25px;
+  text-decoration: none;
+}
+
+.links a:hover {
+  color: burlywood;
+}
+FontAwesomeIcon {
+  color : white;
+}
+</style>
