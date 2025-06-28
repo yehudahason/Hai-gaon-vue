5e2a15879aed6c1cf20ad6775c815c0a741a04d5
yehuda hason <yehudaxt@gmail.com>
2025-06-29 02:13:47 +0300

    paypal

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,7 +21,7 @@
        <form action="https://www.paypal.com/donate" method="post" target="_top">
 <input type="hidden" name="business" value="WY3UA9TDCE5KA" />
 <input type="hidden" name="no_recurring" value="1" />
-<input type="hidden" name="item_name" value="Torah support" />
+<input type="hidden" name="item_name" value="pitron-halomot.org support" />
 <input type="hidden" name="currency_code" value="ILS" />
 <label> <p>תמיכה באתר </p> </label>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
