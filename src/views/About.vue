eee7e418a88ec22c897b73d7bb6ebb98d440a002
yehuda hason <yehudaxt@gmail.com>
2025-06-29 15:41:40 +0300

    paypal3

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="about-container" >
+    <div class="about-container">
         <br />
         <p>מהדורת אינטרנט לספר סדר פתרון חלומות</p>
         <br />
@@ -18,24 +18,24 @@
         </form>
         <br />
         <br />
-       <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
 <input type="hidden" name="business" value="WY3UA9TDCE5KA" />
-<input type="hidden" name="no_recurring" value="1" />
-<input type="hidden" name="item_name" value="pitron-halomot.org support" />
-<input type="hidden" name="currency_code" value="ILS" />
-<label> <p>תמיכה באתר </p> </label>
+<input type="hidden" name="no_recurring" value="0" />
+<input type="hidden" name="item_name" value="For pitron-halomot.org support" />
+<input type="hidden" name="currency_code" value="USD" />
+<label for="submit"><p>תמיכה  באתר </p></label>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 
 </form>
 
-<br/>
-<br/>
-<br />
-<br />
-<br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
 
     </div>
-    <br/>
+    <br />
     <br />
     <div class="banner">
         <b>תפילה תשובה וצדקה</b>
@@ -51,7 +51,7 @@ import Circle from '../components/Circle.vue'
 import { ref } from 'vue'
 const search = ref('')
 const handleclick = (e) => {
-    
+
     window.location.href =
         `https://www.google.com/search?q=site:https://pitron-halomot.org "${search.value}"`
 }
