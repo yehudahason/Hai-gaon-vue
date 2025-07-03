af6c9efc540894c6bfe4fa133029af495671c2b1
yehuda hason <yehudaxt@gmail.com>
2025-07-03 04:36:13 +0300

    paypal2

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,16 +19,16 @@
         <br />
         <br />
         <form action="https://www.paypal.com/donate" method="post" target="_top">
-<input type="hidden" name="business" value="WY3UA9TDCE5KA" />
-<input type="hidden" name="no_recurring" value="0" />
-<input type="hidden" name="item_name" value="For pitron-halomot.org support" />
-<input type="hidden" name="currency_code" value="USD" />
-<label for="submit"><p>תרום לתמיכה באתר </p></label>
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-
-
-
-</form>
+            <input type="hidden" name="business" value="WY3UA9TDCE5KA" />
+            <input type="hidden" name="no_recurring" value="0" />
+            <input type="hidden" name="item_name" value="For pitron-halomot.org support" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <label for="submit">
+                <p>תרום לתמיכה באתר </p>
+            </label>
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0"
+                name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        </form>
 
         <br />
         <br />
