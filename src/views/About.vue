bd2ce7728d61c620de83ae29ec765c391c969b73
yehuda hason <yehudaxt@gmail.com>
2025-07-03 04:35:07 +0300

    paypal2

--- src/views/About.vue
+++ src/views/About.vue
@@ -26,6 +26,8 @@
 <label for="submit"><p>תרום לתמיכה באתר </p></label>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 
+
+
 </form>
 
         <br />
