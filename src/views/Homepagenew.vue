6d35ebd8198f2c055a9d3ae9859966bdcf455d6d
yehuda <yehudaxt@gmail.com>
2025-10-31 12:05:26 +0200

    book links

--- src/views/Homepagenew.vue
+++ src/views/Homepagenew.vue
@@ -1,26 +1,25 @@
 <template>
-  <br/>
-    <div class="home-container">
-       <h1>ספר סדר פתרון חלומות</h1>
+  <br />
+  <div class="home-container">
+    <h1>ספר סדר פתרון חלומות</h1>
     <h3>
       מהרב הגאון רבינו האי ז"ל ויוסף הצדיק ודניאל איש חמודות <br />
       ורש"י ז"ל וחכמים אחרים
     </h3>
     <h3>נדפס לבקשת הבחור יקר ונבון כמ"ר מנחם בכה"ר דוד טרינקי יצ"ו</h3>
-          <h3>דפוס ונציה 5383</h3>
+    <h3>על פי דפוס ונציה 5383</h3>
 
-    <br /> 
-
-      <RouterLink to="/book" class="enter">  כניסה לקריאה  <i class="fa-solid fa-arrow-left"></i></RouterLink>
-      <br/>
-      
-      
-    </div>
-    
     <br />
+
+    <RouterLink to="/book" class="enter">
+      כניסה לקריאה <i class="fa-solid fa-arrow-left"></i
+    ></RouterLink>
+    <br />
+  </div>
+
+  <br />
 </template>
 
 <script setup>
-import {RouterLink} from 'vue-router'
- </script>
-
+import { RouterLink } from "vue-router";
+</script>
