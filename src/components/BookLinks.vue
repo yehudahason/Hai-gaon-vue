6492559bb7de24b3b12aa5618aad8a8612a1c6b2
yehuda <yehudaxt@gmail.com>
2025-10-31 12:01:08 +0200

    book links

---
+++ src/components/BookLinks.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="book-links">
+    <RouterLink to="./book">חלק א' </RouterLink>
+    <RouterLink to="./book-2">חלק ב' </RouterLink>
+    <RouterLink to="./book-3">חלק ג' </RouterLink>
+  </div>
+</template>
--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,23 +1,15 @@
 <template>
-     <div class="links">
-      <RouterLink to="/"><i class="fa-solid fa-house"></i></RouterLink>
-      <RouterLink to="/book"> חלק א'</RouterLink>
-      <RouterLink to="/book-2"> חלק ב'</RouterLink>
-      <RouterLink to="/book-3"> חלק ג'</RouterLink>
-      <RouterLink to="/roe1"> פרק הרואה</RouterLink>
+  <div class="links">
+    <RouterLink to="/"><i class="fa-solid fa-house"></i></RouterLink>
+    <RouterLink to="/book"> פתרון חלומות לרב האי גאון</RouterLink>
+    <RouterLink to="/roe1"> פרק הרואה</RouterLink>
 
-      <RouterLink to="/about">אודות</RouterLink>
-    </div>
-
+    <RouterLink to="/about">אודות</RouterLink>
+  </div>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
-
-
-
+import { RouterLink } from "vue-router";
 </script>
 
-<style >
-
-</style>
+<style></style>
