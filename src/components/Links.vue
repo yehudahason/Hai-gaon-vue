4df050a294ba970855355c1da8e1bc3afd0aa8be
Yehuda Hason <yehudaxt@gmail.com>
2026-01-25 01:57:06 +0200

    forum link

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -7,6 +7,7 @@ import { RouterLink } from "vue-router";
     <RouterLink to="/"><i class="fa-solid fa-house"></i></RouterLink>
     <RouterLink to="/book"> פתרון חלומות לרב האי גאון</RouterLink>
     <RouterLink to="/roe1"> פרק הרואה</RouterLink>
+    <a href="https://forum.pitron-halomot.org"> פורום</a>
     <RouterLink to="/download">הורדות</RouterLink>
 
     <RouterLink to="/about">אודות</RouterLink>
