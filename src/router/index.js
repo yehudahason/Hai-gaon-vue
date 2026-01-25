507de49d593616d657e4b70da645f753974accc5
Yehuda Hason <yehudaxt@gmail.com>
2026-01-25 02:32:22 +0200

    title setup

--- src/router/index.js
+++ src/router/index.js
@@ -22,19 +22,23 @@ const router = createRouter({
       path: "/",
       name: "Home",
       component: Homepagenew,
+      meta: { title: "פתרון חלומות לרב האי גאון" }
     },
 
     {
       path: "/book",
       name: "Book",
       component: Book,
+      meta: { title: "פתרון חלומות לרב האי גאון" },
       alias: "/book.html",
+
     },
 
     {
       path: "/book-2",
       name: "Book2",
       component: Book2,
+      meta: { title: "פתרון חלומות לרב האי גאון" },
       alias: "/book-2.html",
     },
 
@@ -42,66 +46,77 @@ const router = createRouter({
       path: "/book-3",
       name: "Book3",
       component: Book3,
+      meta: { title: "פתרון חלומות לרב האי גאון" },
       alias: "/book-3.html",
     },
     {
       path: "/about",
       name: "About",
       component: About,
+      meta: { title: "אודות פתרון חלומות לרב האי גאון" },
       alias: "/about.html",
     },
-     {
+    {
       path: "/download",
       name: "Download",
       component: Download,
+      meta: { title: "הורדות" },
       alias: "/download.html",
     },
     {
       path: "/roe1",
       name: "Roe1",
       component: Roe1,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe1.html",
     },
     {
       path: "/roe2",
       name: "Roe2",
       component: Roe2,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe2.html",
     },
     {
       path: "/roe3",
       name: "Roe3",
       component: Roe3,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe3.html",
     },
     {
       path: "/roe4",
       name: "Roe4",
       component: Roe4,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe4.html",
     },
     {
       path: "/roe5",
       name: "Roe5",
       component: Roe5,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe5.html",
     },
     {
       path: "/roe6",
       name: "Roe6",
       component: Roe6,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe6.html",
     },
     {
       path: "/roe7",
       name: "Roe7",
       component: Roe7,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe7.html",
     },
     {
       path: "/roe8",
       name: "Roe8",
       component: Roe8,
+      meta: { title: "פרק הרואה מסכת ברכות" },
       alias: "/roe8.html",
     },
 
@@ -117,6 +132,10 @@ const router = createRouter({
     }
     return { top: 0 };
   },
+});
+
+router.beforeEach((to) => {
+  document.title = to.meta.title || "פתרון חלומות לרב האי גאון";
 });
 
 export default router;
