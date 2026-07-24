99d21d70adfd1fe1837125e862707658c92127f3
Yehuda Hason <yehudaxt@gmail.com>
2026-07-24 19:10:33 +0300

    adding moon

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,13 +1,10 @@
 <template>
-  <br />
   <div class="home-container">
     <h1>ספר סדר פתרון חלומות</h1>
     <h3>
       מהרב הגאון רבינו האי ז"ל ויוסף הצדיק ודניאל איש חמודות <br />
       ורש"י ז"ל וחכמים אחרים
     </h3>
-
-    <br />
 
     <RouterLink to="/book" class="enter">
       כניסה לקריאה <i class="fa-solid fa-arrow-left"></i
